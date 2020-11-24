575bffa43d85492170801d5042252b683e406b45
marcuscastilho <marcus@iconexlog.com.br>
2020-11-24 12:03:47 -0300

    adicionado funcionalidades

--- app/views/cadastroUsuario.cs
+++ app/views/cadastroUsuario.cs
@@ -1,48 +1,39 @@
-// using System;
+using System;
 
-// namespace advanced
-// {
-//   // Interface usuário, cadastro
-//   public static class CadastroUsuario
-//   {
-//     public static void Menu()
-//     {
-//       while (true)
-//       {
-//         Console.WriteLine("##############################################");
-//         Console.WriteLine("##########    Cadastro de Usuário    #########");
-//         Console.WriteLine("##############################################\n\n");
-//         Console.Write("Digite seu nome  (obrigatório): ");
-//         string nome = Console.ReadLine();
-//         Console.Write("Digite seu email (obrigatório): ");
-//         string email = Console.ReadLine();
-//         Console.Write("Digite seu CNPJ ou CPF (obrigatório): ");
-//         string cnpj = Console.ReadLine();
-//         Console.Write("Digite seu telefone: ");
-//         string tel = Console.ReadLine();
-//         Console.Write("Defina sua senha (obrigatório): ");
-//         string senha = Console.ReadLine();
+namespace advanced
+{
+  // Interface usuário, cadastro
+  public static class CadastroUsuario
+  {
+    public static void Menu()
+    {
+      while (true)
+      {
+        Console.WriteLine("##############################################");
+        Console.WriteLine("##########    Cadastro de Usuário    #########");
+        Console.WriteLine("##############################################\n\n");
+        Console.Write("Digite seu nome  (obrigatório): ");
+        string nome = Console.ReadLine();
+        Console.Write("Digite seu email (obrigatório): ");
+        string email = Console.ReadLine();
+        Console.Write("Digite seu CNPJ ou CPF (obrigatório): ");
+        string cnpj = Console.ReadLine();
+        Console.Write("Digite seu telefone: ");
+        string tel = Console.ReadLine();
+        Console.Write("Defina sua senha (obrigatório): ");
+        string senha = Console.ReadLine();
 
-//         Console.WriteLine(nome);
-//         Console.WriteLine(email);
-//         Console.WriteLine(cnpj);
-//         Console.WriteLine(tel);
-//         Console.WriteLine(senha);
+        Console.WriteLine(nome);
+        Console.WriteLine(email);
+        Console.WriteLine(cnpj);
+        Console.WriteLine(tel);
+        Console.WriteLine(senha);
 
-//         var res = Usuario.CadastrarUsuario(nome, tel, cnpj, email, senha);
-//         if (res)
-//         {
-//           Console.WriteLine("Cadastrado com sucesso!");
-//           // Direcionar para o login
-//           break;
-//         }
-//         else
-//         {
-//           Console.WriteLine("Falha no cadastro, tente novamente!");
-//         }
-//       }
+        Usuario usuario = Usuario.CadastrarUsuario(nome, tel, cnpj, email, senha);
 
-//     }
+      }
 
-//   }
-// }
+    }
+
+  }
+}
